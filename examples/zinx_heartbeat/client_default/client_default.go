f1f9f2d1f1704cfc1b71dc6f92404a7389ef006d
lh <992610900@qq.com>
2024-09-07 14:02:55 +0800

    测试一下Go语言的环境。

--- examples/zinx_heartbeat/client_default/client_default.go
+++ examples/zinx_heartbeat/client_default/client_default.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"time"
-
-	"github.com/aceld/zinx/znet"
+	"fmt"
 )
 
 func main() {
-	client := znet.NewClient("127.0.0.1", 8999)
+	//client := znet.NewClient("127.0.0.1", 8999)
 
 	// Start heartbeating detection.
-	client.StartHeartBeat(3 * time.Second)
+	//client.StartHeartBeat(3 * time.Second)
 
-	client.Start()
+	//client.Start()
 
 	// wait
-	select {}
+	//select {}
+	fmt.Println("测试Go的环境！")
 }
